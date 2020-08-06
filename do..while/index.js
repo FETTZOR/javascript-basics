f510d1e0272dc8ba26993ecdedc0e404fdf1ba9b
Emil Kh <45365067+FETTZOR@users.noreply.github.com>
2020-08-07 00:27:09 +0300

    dowhile mini exercise

---
+++ do..while/index.js
@@ -0,0 +1,19 @@
+
+
+for (let i = 1; i <= 8; i++){
+    if (i % 2 !== 0) console.log(i);
+}
+
+// let i = 0;
+// while(i <= 8) {
+// if (i % 2 !== 0) console.log(i);
+// i++;
+// }
+
+// do while
+
+let i = 0;
+do {
+if (i % 2 !== 0) console.log(i);
+ i++;
+} while (i <= 5);
