87d1d943d6e44e9c59eed887dd24ff233fd5cad6
Emil Kh <45365067+FETTZOR@users.noreply.github.com>
2020-08-07 10:15:10 +0300

    exercise

---
+++ For...of/index.js
@@ -0,0 +1,15 @@
+
+const person = {
+    name: 'Emil',
+    age: 24
+    };
+    for(let key in person)
+     console.log(key, person[key]);
+    
+    
+
+    // for-of
+
+    const colors = ['red', 'green', 'blue']
+    for(let color of colors)
+     console.log(color);
